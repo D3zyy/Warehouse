4b5532f57d594f241748d747511dc8d7a0716ee5
D3zyy <michaldolezel@gmail.com>
2024-03-10 11:47:00 +0100

    working suppliers editing all atributes

--- src/RowGateway/SupplierRowGateway.py
+++ src/RowGateway/SupplierRowGateway.py
@@ -41,24 +41,41 @@ class SupplierRowGateway(RowGateway):
                         print("Uspesne jste vytvorili noveho dodavatele! \n")
     def update(self):
         while True:
+                #validate id
                 is_id = validate_number("dodavatele")
-                print(is_id)
+                 #check existance of id
                 id_supplier = check_existance_of_id_supplier(is_id,"Suppliers",self.database_connector)
-                print(id_supplier)
+     
                 if id_supplier == True:
                         atribute = choice_atribute()
                         if atribute == 1:
                               new_name_of_supplier =  validate_name("dodavatele")
-                              print(new_name_of_supplier)
+
                               query = "UPDATE Suppliers SET name = %s WHERE supplier_id = %s"
-                              print(f"id upravujici ho ::: {is_id}")
-                              result = self.database_connector.execute_query_with_commit(query, (new_name_of_supplier,is_id))
+                             
+                              self.database_connector.execute_query_with_commit(query, (new_name_of_supplier,is_id))
+                              self.name = new_name_of_supplier
                               print("\nUspesne jste upravili dodavatele!\n") 
                               break
                         elif atribute == 2:
-                              print("adress id")
+                              
+                                new_address_id = validate_number("adresy")
+                                id_address_exist = check_existance_of_id(new_address_id,"Addresses",self.database_connector)
+                                if id_address_exist == True:
+                                        self.address_id = id_address_exist
+                                        query = "UPDATE Suppliers SET address_id = %s WHERE supplier_id = %s"
+                                        self.database_connector.execute_query_with_commit(query, (new_address_id,is_id))
+                                        print("\nUspense jste upravili id adresy u dodavatele!")
+                                        break
+                                        
+                                else:
+                                        print("Toto id neexistuje. Zkuste to znovu.")
                         elif atribute == 3:
-                              print("telefoni cislo")
+                               phone_number = validate_phone_number()
+                               query = "UPDATE Suppliers SET contact_number = %s WHERE supplier_id = %s"
+                               self.database_connector.execute_query_with_commit(query, (phone_number,is_id))
+                               print("\nUspense jste upravili kontaktni telefon dodavatele!\n")
+                               break
                 else:
                     print("Toto id neexistuje. Zkuste to znovu.")
     def delete(self):

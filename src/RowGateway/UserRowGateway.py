bd265aa3c3805b7b2e381667debaa3d8a58812fc
D3zyy <michaldolezel@gmail.com>
2024-03-11 17:40:39 +0100

    added deleting accounts

--- src/RowGateway/UserRowGateway.py
+++ src/RowGateway/UserRowGateway.py
@@ -10,11 +10,82 @@ class UserRowGateway(RowGateway):
         self.username = None
         self.role_id = None
     def create(self):
-        pass
-    def update(self):
-        pass
-    def delete(self,password):
-        pass
+        while True:
+            username =  validate_name()
+            query = "SELECT username from  Users WHERE username = %s"
+            result = self.database_connector.execute_query(query, (username,)) 
+            if result:
+                    print("Uzivatelske jmeno jiz existuje. Zvolte jine jmeno. Zkuste to znovu")
+
+            else:
+                password = validate_password()
+                while True:
+                    role = input("Zadejte roli uzivatele [guest,employee,manager,admin]")
+                    match role:
+                        case "guest":
+                            role = 4
+                            break
+                        case "employee":
+                            role = 3
+                            break
+                        case "manager":
+                            role = 2
+                            break
+                        case"admin":
+                            role = 1
+                            break
+                        case _:
+                            print("Tato role není dostupná. Zkuste to znovu")
+                query = "INSERT INTO Users(username,password,role_id)  VALUES(%s,%s,%s)"
+                self.database_connector.execute_query_with_commit(query, (username,password,role)) 
+                print("\nUspesne jste vytvorili noveho uzivatele!\n")
+                break
+    def update(self,login_mng):
+        while True:
+            choice = input("Zadejte atrivut ktery chcete upravit \n 1) uzivatelske jmeno \n 2) heslo \n 3) role\n")
+            match choice:
+                case "1":
+                    self.change_username(login_mng)
+                    break
+                case "2":
+                    self.change_password(login_mng)
+                    break
+                case "3":
+                    while True:
+                        role = input("Zadejte roli uzivatele [guest,employee,manager,admin]")
+                        match role:
+                            case "guest":
+                                role = 4
+                                break
+                            case "employee":
+                                role = 3
+                                break
+                            case "manager":
+                                role = 2
+                                break
+                            case"admin":
+                                role = 1
+                                break
+                            case _:
+                                print("Tato role není dostupná. Zkuste to znovu")
+                    query = "UPDATE Users SET role_id = %s where user_id = %s "
+                    print(f"id uzivatele prihlaseneho : {login_mng.user_id} menime na roli {role}")
+                    self.database_connector.execute_query_with_commit(query, (role,login_mng.user_id)) 
+                    print("Role byla uspensa zmenena")
+                    break
+
+    def delete(self):
+        while True:
+            username = input("Zadejte uzivatelske jmeno uzivatele ktereho chcete smazat : ")
+            query = "SELECT username from  Users WHERE username = %s"
+            result = self.database_connector.execute_query(query, (username,)) 
+            if result:
+                    query = "DELETE FROM  Users where username = %s "
+                    self.database_connector.execute_query_with_commit(query, (username,)) 
+                    print("Uspense jste smazali uzivatele ze systemu")
+                    break
+            else:
+                print("Toto uzivatelske jmeno neexistuje. Zkuste to znovu")
     def change_username(self,login_mng):
         while True:
             new_name = validate_name()
@@ -37,14 +108,15 @@ class UserRowGateway(RowGateway):
             query = "SELECT password from Users where user_id  = %s"
             result = self.database_connector.execute_query(query, (login_mng.user_id,)) 
             if not result[0][0] == current_password:
-                print("\nHesla se neshodují!\n")
+                print("\nHesla se neshodují! Zkuste to znovu\n")
+                
+            else: 
+                new_password = validate_password()
+                query = "UPDATE Users SET password = %s WHERE user_id = %s"
+                self.database_connector.execute_query_with_commit(query, (new_password,login_mng.user_id)) 
+                login_mng.password = new_password
+                print("\nHeslo bylo uspesne zmeneno\n")
                 break
-            new_password = validate_password()
-            query = "UPDATE Users SET password = %s WHERE user_id = %s"
-            self.database_connector.execute_query_with_commit(query, (new_password,login_mng.user_id)) 
-            login_mng.password = new_password
-            print("\nHeslo bylo uspesne zmeneno\n")
-            break
     def delete_account(self,login_mng):
         while True:
             confirmation = input(" Jste si jisty ze chcete smazat svuj ucet pro potvrzeni napiste : smazat \n Pro storno napise : storno\n")
@@ -52,7 +124,7 @@ class UserRowGateway(RowGateway):
                 case "smazat":
                     query = "DELETE FROM Users WHERE user_id = %s"
                     self.database_connector.execute_query_with_commit(query, (login_mng.user_id,))
-                    print("\nUzivatelsky ucet byl uspesne smazan\n Byli jste odhlaseni \n")
+                    print("\nUzivatelsky ucet byl uspesne smazan\nByli jste odhlaseni \n")
                     login_mng.log_out()
                     break
                 case "storno":
--- src/menu_manager/account_manager.py
+++ src/menu_manager/account_manager.py
@@ -17,6 +17,9 @@ class account_manager:
             case "3":
                 user_row.delete_account(login_mng)
             case _:
+                #user_row.update(login_mng)
+                #user_row.create()
+                user_row.delete()
                 print("\nTato volba není dostupná \n")
     def print_account_options(self,path_to_account_options):
         with open(path_to_account_options) as json_file:
